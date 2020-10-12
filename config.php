1d75cf8f8d6fbaf67c87afa2d8898b8c1755dae7
Bernard Sheppard <37993507+BJReplay@users.noreply.github.com>
2020-10-12 17:04:32 +1100

    Update config.php

--- config.php
+++ config.php
@@ -3,12 +3,6 @@ session_start();
 
 include_once("functions.php");
 
-// Database Details
-$host = "dbhost"; // MySQL Hostname
-$user = "dbuser"; // MySQL Username
-$pass = "dbpassword"; // MySQL Password
-$dbname = "bsheartrate"; // MySQL DB Name
-
 //below  will give the whole connectionstring in a single string
 $conn = getenv("MYSQLCONNSTR_localdb"); 
 
@@ -20,29 +14,24 @@ foreach($conarr2 as $key=>$value){
     $conarr[$k] = substr($value,strpos($value,'=')+1);
 }
 
-$host = $conarr["Data Source"]; // MySQL Hostname
-$user = $conarr["User Id"]; // MySQL Username
-$pass = $conarr["Password"]; // MySQL Password
+$host = $conarr["Data Source"]; 
+$user = $conarr["User Id"]; 
+$pass = $conarr["Password"];
 $dbname = $conarr["Database"];
-
 
 // Other Configuration Parameters
 
 // Replace the Redirect URL with your setup
-$config_redirect_uri = "https://exain.com/fitbit/token.php";
-
+$config_redirect_uri = "https://bsheartrate.azurewebsites.net/token.php";
 
 $config_oauth_url = "https://www.fitbit.com/oauth2/authorize";
 $config_scope = "heartrate profile"; // only getting heartrate and profile data
 $config_expires_sec = 30 * 24 * 60 * 60; // 30 Days
 $config_api_url = "https://api.fitbit.com/1/user/-/activities/heart/date"; // please see getdata.php /$date/1d/1sec/time/00:00/23:59.json";
 
-
 // Connecting to Database
 $config_conn = mysqli_connect("$host","$user","$pass","$dbname");
-//$config_conn = mysqli_connect($conarr["Data Source"],$conarr["User Id"],$conarr["Password"],"bsheartrate");
 
 if (!$config_conn) { exit("No MySQL Connection Established.<BR><BR>" . mysqli_error($config_conn)); }
-
 
 ?>
