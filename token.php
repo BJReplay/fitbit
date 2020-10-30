e1c7f2ba4aeef1dc3d603d8fe1362c157ca78a2d
Bernard Sheppard <37993507+BJReplay@users.noreply.github.com>
2020-10-30 12:02:54 +1100

    Update token.php

--- token.php
+++ token.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once("config.php");
+
 // Step 1 - Fitbit provides data in "#" which PHP cannot easily interpret
 // So doing some Javascript magic and redirecting it in a way that PHP can interpret it
 if (!isset($_GET['hash'])) {
@@ -22,7 +24,7 @@ if (!isset($_GET['hash'])) {
 
 	// Step 2 - Now the data is in query string. Initialize Session and process it
 	$access_token = $_GET['access_token'];
-	setcookie("fb_access_token", base64_encode($access_token), time()+(60 * 60 * 24 * 30), "/", "bsheartrate.azurewebsites.net", true, true); 
+	setcookie("fb_access_token", base64_encode($access_token), time()+$config_expires_sec, "/", "bsheartrate.azurewebsites.net", true, true); 
 	session_start();
 	$_SESSION['fb_access_token'] = $access_token;
 	header("Location: data.php"); // redirect to data review page
