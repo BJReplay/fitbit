ad5cafd829605a8d8bd703fcc135e707fc9d78b5
Bernard Sheppard <37993507+BJReplay@users.noreply.github.com>
2023-12-20 10:25:45 +1100

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ session_start();
 include_once("functions.php");
 
 //below  will give the whole connectionstring in a single string
-$conn = getenv("MYSQLCONNSTR_localdb"); 
+$conn = getenv("AZURE_MYSQL_CONNECTIONSTRING"); 
 
 //Let's split it and decorate it in an array
 $conarr2 = explode(";",$conn); 
@@ -22,7 +22,7 @@ $dbname = $conarr["Database"];
 // Other Configuration Parameters
 
 // Replace the Redirect URL with your setup
-$config_redirect_uri = "https://bsheartrate.azurewebsites.net/token.php";
+$config_redirect_uri = "https://bsheartratenew.azurewebsites.net/token.php";
 
 $config_oauth_url = "https://www.fitbit.com/oauth2/authorize";
 $config_scope = "heartrate profile oxygen_saturation respiratory_rate sleep temperature"; // only getting heartrate and profile data
