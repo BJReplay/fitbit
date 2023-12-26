f02a43d191835fa30549d01fa8f5c4cbf330b7cc
BJReplay <37993507+BJReplay@users.noreply.github.com>
2023-12-26 11:26:04 +1100

    Update config.php

--- config.php
+++ config.php
@@ -14,7 +14,12 @@ foreach($conarr2 as $key=>$value){
     $conarr[$k] = substr($value,strpos($value,'=')+1);
 }
 
-$host = $conarr["Server"]; 
+//$host = $conarr["Server"]; 
+//$user = $conarr["User Id"]; 
+//$pass = $conarr["Password"];
+//$dbname = $conarr["Database"];
+
+$host = $conarr["Data Source"]; 
 $user = $conarr["User Id"]; 
 $pass = $conarr["Password"];
 $dbname = $conarr["Database"];
@@ -31,12 +36,12 @@ $config_expires_sec = 30 * 24 * 60 * 60; // 30 Days
 $config_api_url = "https://api.fitbit.com/1/user/-/activities/heart/date"; // please see getdata.php /$date/1d/1sec/time/00:00/23:59.json";
 
 // Connecting to Database
-$config_conn = mysqli_init();
-$config_conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
-$config_conn->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-$config_conn->real_connect("$host","$user","$pass","$dbname");
+//$config_conn = mysqli_init();
+//$config_conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+//$config_conn->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+//$config_conn->real_connect("$host","$user","$pass","$dbname");
 //$mysqli->close();
-//$config_conn = mysqli_connect("$host","$user","$pass","$dbname");
+$config_conn = mysqli_connect("$host","$user","$pass","$dbname");
 
 if (!$config_conn) { exit("No MySQL Connection Established.<BR><BR>" . mysqli_error($config_conn)); }
 
